e972b01846eb57f6354f903e7bd6c0bdabd7572b
= <=>
2020-07-14 12:11:27 +0200

    Updates

---
+++ precache-manifest.a81aa0e66c9c1b6532affe2b811a193c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33bc1a576942a466998ced0ac865db04",
+    "url": "/ranker/index.html"
+  },
+  {
+    "revision": "6f316db549791b2b19e8",
+    "url": "/ranker/static/css/main.67663238.chunk.css"
+  },
+  {
+    "revision": "ed3c88ec7e636dc5fb20",
+    "url": "/ranker/static/js/2.1765f6a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ranker/static/js/2.1765f6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f316db549791b2b19e8",
+    "url": "/ranker/static/js/main.f5982a87.chunk.js"
+  },
+  {
+    "revision": "060357c38bd43fb6261e",
+    "url": "/ranker/static/js/runtime-main.5a9ed4df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ranker/static/media/logo.5d5d9eef.svg"
+  }
+]);
